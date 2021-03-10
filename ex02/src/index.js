2494ccf71aef845ffcd306b880125205e22a7825
Adis Salihovic <adis.braco@gmail.com>
2021-03-10 19:24:52 +0100

    Saving JS and HTML files

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  // Only change code below this line
+  // This is a one line comment
+  /* This is a
+    multi line comment 
+    and everything written in here
+    will be ignored by the JS
+    */
+  // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
